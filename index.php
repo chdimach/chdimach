5c84d694ce69721b91ee3d11797ec2917540cd36
ilyagrinyk <ilyagrinyk@gmail.com>
2020-04-06 23:45:43 +0300

    изменение в логической структуре Н1 = Н2; Н3 = Н2.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include ('header.php');
 	<div class="container">
 		<div class="translations-wrap text-center text-sm-left">
 			<div class="translations-header">
-				<h1 class="translations-header-title">Переводы Библии</h1>
+				<h2 class="group-name">Переводы Библии</h2>
 			</div>
 			<nav class="translations-nav">
 				<div class="translations-items d-flex flex-wrap justify-content-center justify-content-sm-start">
@@ -15,11 +15,11 @@ include ('header.php');
                     $translations = get_translations();
                     ?>
                     <?php foreach ($translations as $translation):?>
-                        <h3 class="translation-item ">
+                        <h2 class="translation-item ">
                             <a class="translation-link" href="?translation =<?=$translation['type'] ?>">
                                 <span class="translation-title"><?=$translation['name'] ?></span>
                             </a>
-                        </h3>
+                        </h2>
                     <?php endforeach;  ?>
 				</div>
 			</nav>
@@ -28,7 +28,7 @@ include ('header.php');
 <div class="container">
 	<div>
 		<div class="d-flex align-items-center justify-content-center mt-5 mb-2">
-			<h3 class="group-name">Ветхий завет</h3><span class="pipe">|</span><small>Синодальный перевод</small>
+			<h2 class="group-name">Ветхий завет</h2><span class="pipe">|</span><small>Синодальный перевод</small>
 		</div>
 		<div class="books-wrap">
 			<div class="books d-flex flex-wrap justify-content-center">
@@ -47,7 +47,7 @@ include ('header.php');
 	</div>
 	<div>
 		<div class="d-flex align-items-center justify-content-center mt-5">
-			<h3 class="group-name">Новый завет</h3><span class="pipe">|</span><small>Синодальный перевод</small>
+			<h2 class="group-name">Новый завет</h2><span class="pipe">|</span><small>Синодальный перевод</small>
 		</div>
 		<div class="books-wrap">
 			<div class="books d-flex flex-wrap justify-content-center">
