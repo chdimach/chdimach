1e0fe8792f35ef730f1574d4375cdf02ae6c79f8
taraslis453 <taraslisiy54@gmail.com>
2020-04-16 08:56:01 +0300

    Правки

--- read.php
+++ read.php
@@ -74,7 +74,8 @@ function nextback($type){
 						<div class="d-none d-lg-block">
                             <?=$book_name[0]['name']?>
 						</div>
-						<div class="d-lg-none">
+						<div class="d-md-none">Книги</div>
+						<div class="d-none d-md-block d-lg-none">
 							<span class="book-abbr"><?=$book_name[0]['short_name']?></span>
 						</div>
 						<svg class="dropdown-icon" width="16px" height="16px" viewBox="0 0 451.847 451.847">
@@ -90,7 +91,7 @@ function nextback($type){
 								<div class="bible-side-tab bible-side-tab-old current" onclick="toggleTab(this)">Ветхий завет</div>
 							</div>
 							<div class="bible-books-new d-none">
-								<div class="bible-books-wrap mt-lg-0 mt-2 d-flex justify-content-center flex-lg-column flex-wrap">
+								<div class="bible-books-wrap mt-lg-0 mt-2 d-flex justify-content-center flex-column">
                                     <?php
                                     $books = get_books();
                                     $transl='syn';
@@ -104,7 +105,7 @@ function nextback($type){
 								</div>
 							</div>
 							<div class="bible-books-old">
-								<div class="bible-books-wrap bible-books-wrap-old mt-lg-0 mt-2 d-flex justify-content-center flex-lg-column flex-wrap">
+								<div class="bible-books-wrap bible-books-wrap-old mt-lg-0 mt-2 d-flex justify-content-center flex-column">
                                     <?php foreach ($books as $book):?>
                                         <?php if($book['book_number']<40){
                                             echo '<a href="chapters.php?trans='.$transl.'&number_book='.$book["book_number"].'& name='.$book['name'].'" class="bible-book"><span>'.$book['name'] .'</span></a>';
@@ -116,20 +117,22 @@ function nextback($type){
 					</div>
 				</div>
 
-
-				<div class="current-chapter-wrap d-flex align-items-center bible-nav-item" onclick="chapterDropdown()">
+				<div class="d-none d-md-block">
+					<div class="current-chapter-wrap d-flex align-items-center bible-nav-item" onclick="chapterDropdown()">
 						<div class="current-chapter chapter-select-item d-flex"><span>Глава </span><span class="chapter-number"><?=$content[0]['number_chapter'] ?></span></div>
 						<svg class="dropdown-icon" width="16px" height="16px" viewBox="0 0 451.847 451.847">
 							<path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751
-                        c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0
-                        c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+				                        c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0
+				                        c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
 						</svg>
-					<div class="chapter-dropdown-list d-flex flex-column">
-                        <?php foreach ($all_chapters_current_book as $chap):?>
-                            <div class="chapter-select-item"><a class="chapter-number" href="read.php?chapter=<?=$chap['number_chapter'] ?>&number=<?= $chap['book_number']?>"><span>Глава</span><?=$chap['number_chapter']?></a></div>
-                        <?php endforeach; ?>
+						<div class="chapter-dropdown-list d-flex flex-column">
+				            <?php foreach ($all_chapters_current_book as $chap):?>
+								<div class="chapter-select-item"><a class="chapter-number" href="read.php?chapter=<?=$chap['number_chapter'] ?>&number=<?= $chap['book_number']?>"><span>Глава</span><?=$chap['number_chapter']?></a></div>
+				            <?php endforeach; ?>
+						</div>
 					</div>
 				</div>
+
 
 				<div class="translation-select-wrap d-flex flex-column align-items-center justify-content-center bible-nav-item">
 					<div class="translation-wrap-current d-flex align-items-center justify-content-center" onclick="translationsDropdown()">
@@ -172,6 +175,30 @@ function nextback($type){
                     </div>
                 <?php endforeach;  ?>
             </div>
+
     </div>
+	<div class="d-flex justify-content-between container mt-4 p-3 bible-nav-bottom">
+		<a class="d-flex align-items-center" href="<?php echo nextback('back');?>">
+			<div class="arrow-back">
+				<svg fill="#fafafa" x="0px" y="0px" width="14px" height="14px" viewBox="0 0 451.846 451.847" style="transform: rotate(180deg)">
+					<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+                                L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+                                c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"/>
+				</svg>
+			</div>
+			<span class="ml-1 d-none d-md-block">Предыдущая глава</span>
+		</a>
+		<a class="d-flex align-items-center" href="<?php echo nextback('next'); ?>">
+			<span class="mr-1 d-none d-md-block">Следущая глава</span>
+			<div class="arrow-next">
+				<svg fill="#fafafa" x="0px" y="0px" width="14px" height="14px" viewBox="0 0 451.846 451.847">
+					<path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+                                L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+                                c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"/>
+				</svg>
+			</div>
+
+		</a>
+	</div>
 </main>
 <?php include ('footer.php')?>
